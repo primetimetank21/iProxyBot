23f2bb1ca28052d37bfb75eba8cefc88044da54f
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-05-18 16:49:06 -0400

    feat: new function to clear terminal; all output cleared at end of runtime

--- progTest.py
+++ progTest.py
@@ -13,14 +13,15 @@ def progress(start_str, prog, total):
     else:
         percent = 100
         color   = "green"
-        end     = "\n"
+        # end     = "\n"
+        end     = "\r"
 
     #format percent to be 7 characters every time (important for formatting)
     percent_str = f"{float(percent):.2f}"
     percent_str = percent_str + "%" + (" " * (7 - len(percent_str) - 1))
 
     #start generating printable string -- includes start_str, percent_str, and the progress bar
-    printable_str  = f"\r{start_str:<33} {percent_str} "
+    printable_str  = f"\r{start_str:<2} {percent_str} "
     printable_str += "|"
 
     #create progress bar string
@@ -34,6 +35,14 @@ def progress(start_str, prog, total):
 
     print(printable_str,end=end,flush=True)
 
+def clear_terminal(delay=0.5):
+    """
+    Clears text from the latest terminal line
+    """
+    width = os.get_terminal_size().columns
+    time.sleep(delay)
+    print(" " * width, end="\r")
+
 # def old_progress(start_str, prog, total):
 #     if prog > total: return
 #     percent = 100 * float(prog/total) if prog <= total else 100
@@ -43,6 +52,8 @@ def progress(start_str, prog, total):
 #     print(f"\r{start_str:<33}|{bar}| {percent:.2f}%",end=end)
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     term_size = os.get_terminal_size()
     WIDTH     = term_size.columns
     arr       = list(range(0,10000))
@@ -51,5 +62,10 @@ if __name__ == "__main__":
         sq = num**2
         arr[i] = sq
         progress("Nums", i+1, len(arr))
-        time.sleep(0.0005)
-    print("Done")
+        time.sleep(0.0001)
+
+    end_time   = time.time()
+    clear_terminal()
+    print(f"(Finished in {round(end_time - start_time, 2)} seconds)",end="\r")
+    time.sleep(3)
+    clear_terminal()
