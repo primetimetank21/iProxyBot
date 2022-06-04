38bb1101416d3b150c3db02bf287e6298e7ccc8e
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-06-04 10:11:30 -0400

    refactor: removed final alternate version of 'get_IPs.py' (uses async code)

--- get_IPs.py
+++ get_IPs.py
@@ -3,16 +3,17 @@ import time
 import glob
 import os
 import requests
-import concurrent.futures
+import asyncio
+import aiohttp
+from aiohttp_socks import ProxyConnector
+from fake_useragent import UserAgent
 from threading import Lock
-from typing import Dict
 from bs4 import BeautifulSoup as BS
 from termcolor import colored
 
 #TODO: make this into a class
 
 #functions
-
 def progress(start_str:str, prog:int, total:int) -> None:
     """
     Displays status of task
@@ -111,45 +112,44 @@ def get_ips_on_page(response:requests.Response) -> list:
 
     return ip_list
 
-def _test_ips(start_str:str, ip_dict:Dict, n:int, lock:Lock, num_lock:Lock) -> None:
-    try:
-        # ip_dict = { "ip": ip, "port": port, "type": _type}
-        check_url = "http://httpbin.org/ip"
-        proxy     = f"{ip_dict['ip']}:{ip_dict['port']}"
-        proxies   = {"http": f"http://{proxy}", "https": f"http://{proxy}"}
-        requests.get(check_url, proxies=proxies, timeout=7)
-        types = ip_dict["type"].replace(" ", "").split(",")
-        with lock:
-            global working_IPs
-            for t in types:
-                if t not in working_IPs:
-                    working_IPs[t] = []
-                working_IPs[t].append(f"{t}\t{ip_dict['ip']}\t{ip_dict['port']}")
-    except Exception as e:
-        # write exception in a log file in future?
-        # print(e,end="\r")
-        pass
-    finally:
-        with num_lock:
-            global prog
-            prog += 1
-            progress(f"{start_str} (IPs:{n})", prog, n)
+async def _test_ips(start_str:str, ip_dict:dict, n:int, num_lock:Lock) -> list:
+    check_url = "http://httpbin.org/ip"
+    proxy = f"{ip_dict['ip']}:{ip_dict['port']}"
+    types = ip_dict["type"].replace(" ", "").split(",")
+    r_sesh_json_responses = []
+    for _type in types:
+        _type = _type.replace('s','') if _type == "https" else _type
+        proxy_connector = ProxyConnector.from_url(f"{_type}://{proxy}")
+        async with aiohttp.ClientSession(connector=proxy_connector, timeout=aiohttp.ClientTimeout(total=15)) as session:
+            try:
+                headers = {"User-Agent": UserAgent().random}
+                r_sesh = await session.get(check_url, headers=headers)
+                r_sesh_json = await r_sesh.json()
+                r_sesh_json_responses.append(f"{_type}\t{r_sesh_json['origin']}\t{ip_dict['port']}")
+            except Exception as e:
+                r_sesh_json_responses.append(f"fail\t{str(e)}")
+    with num_lock:
+        global prog
+        prog += 1
+        progress(f"{start_str} (IPs:{n})", prog, n)
 
-def test_ips(master_list:list) -> None:
+    return r_sesh_json_responses
+
+async def test_ips(master_list:list) -> list:
     """
     Tests the IP addresses in the provided list
     """
     global prog
-    global idx
-    prog,idx = 0,0
-    lock     = Lock()
-    num_lock = Lock()
-    params   = [("Testing Proxies", ip_dict, len(master_list), lock, num_lock) for ip_dict in master_list]
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(lambda f: _test_ips(*f), params, timeout=10)
+    prog = 0
 
-def test_old_ips() -> None:
+    num_lock = Lock()
+    tasks    = [asyncio.create_task(_test_ips("Testing Proxies", ip_dict, len(master_list), num_lock)) for ip_dict in master_list]
+    ips      = await asyncio.gather(*tasks)
+
+    clear_terminal(0)
+    return ips
+
+async def test_old_ips() -> list:
     """
     Tests the old IP addresses already saved
     """
@@ -163,54 +163,77 @@ def test_old_ips() -> None:
                 master_list.append(ip_dict)
 
     global prog
-    global idx
-    prog,idx = 0,0
-    lock     = Lock()
+    prog = 0
+
     num_lock = Lock()
-    params   = [("Testing Old Proxies", ip_dict, len(master_list), lock, num_lock) for ip_dict in master_list]
+    tasks    = [asyncio.create_task(_test_ips("Testing Old Proxies", ip_dict, len(master_list), num_lock)) for ip_dict in master_list]
+    ips      = await asyncio.gather(*tasks)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(lambda f: _test_ips(*f), params, timeout=10)
+    #reset the proxy_server files
+    for file_name in glob.glob("*proxy_servers.txt"):
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write("")
 
-def save_ips(ip_dict:dict) -> None:
+    clear_terminal(0)
+    return ips
+
+def remove_logs(file_names:list) -> None:
+    for file_name in file_names:
+        if glob.glob(file_name):
+            os.remove(file_name)
+
+def combine_ip_lists(old_ips:list | None, new_ips:list | None) -> list:
+    all_ips = []
+    if old_ips and new_ips:       all_ips = old_ips + new_ips
+    elif old_ips and not new_ips: all_ips = old_ips
+    elif new_ips and not old_ips: all_ips = new_ips
+
+    return all_ips
+
+def save_ips(all_ips:list) -> None:
     """
     Saves working proxies to files
     """
     print("Saving proxies",flush=True,end="\r")
     clear_terminal()
-    for _type in ip_dict.keys():
-            with open(f"new_my_{_type}_proxy_servers.txt", "w", encoding="utf-8") as f:
-                for i,ip in enumerate(ip_dict[_type]):
-                    f.write(f"{ip}\n")
-                    progress(f"Saving {_type}",i+1,len(ip_dict[_type]))
-            clear_terminal(0.5)
+    for ip_list in all_ips:
+        for ip in ip_list:
+            _type = ip.split('\t')[0]
+            if _type == "fail":
+                with open(f"log.txt", "a") as f:
+                    f.write(ip + '\n')
+            else:
+                with open(f"new_my_{_type}_proxy_servers.txt", "a") as f:
+                    f.write(ip + '\n')
+    clear_terminal(0)
 
-def main() -> None:
+async def main() -> None:
     """
     Driver code
     """
     try:
+        #remove old log files
+        remove_logs(["log.txt"])
+
+        #test old IPs
+        old_ips = await test_old_ips()
+
         #get new IPs and add them to a list
         ip_master_list = get_all_ips(requests.Session())
 
         #test new IPs
-        test_ips(ip_master_list)
+        new_ips = await test_ips(ip_master_list)
 
-        #test old IPs
-        test_old_ips()
-
-        #save successful IPs
-        save_ips(working_IPs)
+        #save ips
+        save_ips(combine_ip_lists(old_ips,new_ips))
 
     except Exception as e:
         print(e)
 
-
 if __name__ == "__main__":
-    working_IPs:Dict[str,list] = {}
-    prog,idx   = 0,0
+    prog = 0
     start_time = time.time()
-    main()
+    asyncio.run(main())
     end_time = time.time()
     print(f"(Finished in {round(end_time - start_time, 2)} seconds)",end="\r")
     clear_terminal(5)
